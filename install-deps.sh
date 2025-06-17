eea7885a0a4c0c9709b347aa664cf1186a67c3dd
AskiaDeLarge <83979339+askia1@users.noreply.github.com>
2025-06-17 12:19:02 -0400

    Update install-deps.sh

--- install-deps.sh
+++ install-deps.sh
@@ -5,7 +5,9 @@ echo "Installing OSINT tool dependencies..."
 sudo apt update && sudo apt install -y nodejs npm
 
 npm install puppeteer-extra puppeteer-extra-plugin-stealth \
-  cheerio jsdom compromise chrono-node libphonenumber-js
+  cheerio jsdom compromise chrono-node libphonenumber-js electron
+
+  
 
 echo "Done. All core Node.js packages installed."
 
